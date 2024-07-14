97a7c5a372d77b3cf09e4a396550aca072bcc6c4
Alex Hamilton-Smith <alexhamiltonsmith@gmail.com>
2024-07-14 20:17:15 +1000

    Fix disk usage calculation in green-elm-title-app.py and update service links in index.html

--- green-elm-title-app.py
+++ green-elm-title-app.py
@@ -4,7 +4,10 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 def get_disk_usage():
-    disk_usage = psutil.disk_usage('/home')
+    try:
+        disk_usage = psutil.disk_usage('/home')
+    except FileNotFoundError:
+        disk_usage = psutil.disk_usage('/')
     total = disk_usage.total // (2**30)  # Convert bytes to gigabytes
     used = disk_usage.used // (2**30)
     free = disk_usage.free // (2**30)
