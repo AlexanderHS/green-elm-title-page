10d41defb3926ab7321b9e0ec628ce70f9ef2d46
Alex Hamilton-Smith <alexhamiltonsmith@gmail.com>
2024-08-11 13:41:08 +1000

    do git reset --hard if needed

--- start.sh
+++ start.sh
@@ -8,11 +8,17 @@ chmod 600 /home/oem/.ssh/id_rsa || exit
 
 # Pull the latest changes from the repository
 git pull || exit
+git pull || {
+    echo "Git pull failed. Performing hard reset..."
+    git reset --hard
+    git pull
+} || exit
 
 # Install the required Python packages
 /home/oem/green-elm-title-page/venv/bin/python -m pip install -r requirements.txt || exit
 
 # Run the Flask application
 /home/oem/green-elm-title-page/venv/bin/python green-elm-title-app.py || exit
+
 
 
